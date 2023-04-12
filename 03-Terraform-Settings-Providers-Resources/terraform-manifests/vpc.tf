57c1f2fd07dadd1e2868f56d6da9aaeb9b696153
Hyeonmin Kim <khm970514@gmail.com>
2023-04-12 15:44:19 +0900

    update outbound group

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/vpc.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/vpc.tf
@@ -32,3 +32,13 @@ resource "aws_security_group_rule" "webserver-sg-http" {
     security_group_id = aws_security_group.webserver-sg.id
     description = "http"
 }
+
+resource "aws_security_group_rule" "webserver-sg-outbound" {
+    type = "egress"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [ "0.0.0.0/0" ]
+    security_group_id = aws_security_group.webserver-sg.id
+    description = "outbound"
+}

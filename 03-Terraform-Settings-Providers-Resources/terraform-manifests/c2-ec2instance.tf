f02d7117ad3ee70213519264f3df09ff7ed67e09
Hyeonmin Kim <khm970514@gmail.com>
2023-04-12 14:22:40 +0900

    update  ami, subnet id, tag

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
@@ -1,9 +1,11 @@
 # Resource: EC2 Instance
 resource "aws_instance" "myec2vm" {
-  ami = "ami-0742b4e673072066f"
+  ami = "ami-09cd5dd529c3c1f40"
   instance_type = "t3.micro"
   user_data = file("${path.module}/app1-install.sh")
-  tags = {
+  subnet_id = aws_subnet.example.id
+  tags = {  
     "Name" = "EC2 Demo"
+    "hmkim" = "EC2"
   }
 }
